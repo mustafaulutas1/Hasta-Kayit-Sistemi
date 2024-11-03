f452c1aea0f62033f8e12417643495877ced2763
Mustafa ULUTAŞ <128393765+mustafaulutas1@users.noreply.github.com>
2024-11-03 14:34:23 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp14/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿
+namespace WindowsFormsApp14
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(929, 208);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 284);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 316);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(128, 344);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(128, 421);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(128, 381);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(128, 530);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(128, 491);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(128, 454);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(128, 558);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 35);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(671, 485);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(671, 530);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 36);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 318);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Tc No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Adres";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 424);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Telefon";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 458);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 17);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Dogum Yeri";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 494);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 17);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Hastalık";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 530);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 17);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "İlaç";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(40, 563);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 17);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Tarih";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(631, 318);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 23;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(755, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 27);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Bilgileri Getir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(318, 326);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(251, 17);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Güncellemek İstediğiniz Hata Tc No su";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(295, 25);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(274, 17);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "HASTA KAYIT SİSTEMİNE HOŞ GELDİNİZ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(954, 586);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ WindowsFormsApp14/Form1.cs
@@ -0,0 +1,309 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Drawing; 
+using System.Linq; 
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        private NpgsqlConnection baglanti;
+        public Form1()
+        {
+            InitializeComponent();
+            Baglan(); 
+            HastaBilgileriniGoster(); // Hastaları göster
+        }
+        private void CustomizeUI()
+        {
+            
+            this.BackColor = Color.LightGray;
+
+            
+            var buttons = this.Controls.OfType<Button>();
+            foreach (var button in buttons)
+            {
+                button.BackColor = Color.Teal;
+                button.ForeColor = Color.White;
+            }
+
+            dataGridView1.BackgroundColor = Color.White;
+
+            this.Text = "Hasta Kayıt Uygulaması";
+            
+        }
+        private void Baglan()
+        {
+            baglanti = new NpgsqlConnection("server=localhost; port=5432; database=hastane;User Id=postgres;password=123456;");
+            try
+            {
+                baglanti.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı hatası: " + ex.Message);
+            }
+        }
+        // Hastaları gösterme 
+        private void HastaBilgileriniGoster()
+        {
+            using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("SELECT * FROM hasta", baglanti))
+            {
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+        // Hasta ekle
+        private void HastaEkle(string ad, string soyad, DateTime dogumTarihi, string dogumYeri, string tcKimlik, string telefon, string hastalik, string ilac, string adres)
+        {
+            try
+            {
+                using (NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO hasta (ad, soyad, dogum_tarihi, dogum_yeri, tc_kimlik, telefon, hastalik, ilac, adres) VALUES (@ad, @soyad, @dogum_tarihi, @dogum_yeri, @tc_kimlik, @telefon, @hastalik, @ilac, @adres)", baglanti))
+                {
+                    cmd.Parameters.AddWithValue("@ad", ad);
+                    cmd.Parameters.AddWithValue("@soyad", soyad);
+                    cmd.Parameters.AddWithValue("@dogum_tarihi", dogumTarihi);
+                    cmd.Parameters.AddWithValue("@dogum_yeri", dogumYeri);
+                    cmd.Parameters.AddWithValue("@tc_kimlik", tcKimlik);
+                    cmd.Parameters.AddWithValue("@telefon", telefon);
+                    cmd.Parameters.AddWithValue("@hastalik", hastalik);
+                    cmd.Parameters.AddWithValue("@ilac", ilac);
+                    cmd.Parameters.AddWithValue("@adres", adres);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Hasta kaydı başarıyla eklendi.");
+            }
+            catch (Exception ex)//Hatalı ekleme
+            {
+                MessageBox.Show("Hasta kaydı eklenemedi HATA!!!: " + ex.Message);
+            }
+            finally
+            {
+                HastaBilgileriniGoster(); // Hasta güncelle
+            }
+        }
+
+        // Hasta sil
+        private void HastaSil(string tc_kimlik)
+        {
+            try
+            {
+                using (NpgsqlCommand cmd = new NpgsqlCommand("DELETE FROM hasta WHERE tc_kimlik = @tc_kimlik", baglanti))
+                {
+                    cmd.Parameters.AddWithValue("@tc_kimlik", tc_kimlik);
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Hasta kaydı silindi.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hasta kaydı silinemedi HATA!!!: " + ex.Message);
+            }
+            finally
+            {
+                HastaBilgileriniGoster(); 
+            }
+        }
+
+
+        // Hasta güncelle
+        private void HastaGuncelle(int hastaID, string ad, string soyad, DateTime dogumTarihi, string dogumYeri, string tcKimlik, string telefon, string hastalik, string ilac, string adres)
+        {
+            try
+            {
+                using (NpgsqlCommand cmd = new NpgsqlCommand("UPDATE hasta SET ad = @ad, soyad = @soyad, dogum_tarihi = @dogum_tarihi, dogum_yeri = @dogum_yeri, telefon = @telefon, hastalik = @hastalik, ilac = @ilac, adres = @adres WHERE tc_kimlik= @tc_kimlik", baglanti))
+                {
+
+                    cmd.Parameters.AddWithValue("@ad", ad);
+                    cmd.Parameters.AddWithValue("@soyad", soyad);
+                    cmd.Parameters.AddWithValue("@dogum_tarihi", dogumTarihi);
+                    cmd.Parameters.AddWithValue("@dogum_yeri", dogumYeri);
+                    cmd.Parameters.AddWithValue("@tc_kimlik", tcKimlik);
+                    cmd.Parameters.AddWithValue("@telefon", telefon);
+                    cmd.Parameters.AddWithValue("@hastalik", hastalik);
+                    cmd.Parameters.AddWithValue("@ilac", ilac);
+                    cmd.Parameters.AddWithValue("@adres", adres);
+
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Hasta kaydı güncellendi.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hasta kaydı güncellenemedi HATA!!!: " + ex.Message);
+            }
+            finally
+            {
+                HastaBilgileriniGoster(); 
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad = textBox1.Text;
+            string soyad = textBox2.Text;
+            DateTime dogumTarihi = dateTimePicker1.Value;
+            string dogumYeri = textBox5.Text;
+            string tc_kimlik = textBox3.Text;
+            string telefon = textBox6.Text;
+            string hastalik = textBox7.Text;
+            string ilac = textBox8.Text;
+            string adres = textBox4.Text;
+
+            HastaEkle(ad, soyad, dogumTarihi, dogumYeri, tc_kimlik, telefon, hastalik, ilac, adres);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                if (dataGridView1.SelectedCells.Count > 0)
+                {
+                    int selectedRowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                    string tc_kimlik = dataGridView1.Rows[selectedRowIndex].Cells["tc_kimlik"].Value.ToString();
+
+                    HastaSil(tc_kimlik);
+                }
+                else
+                {
+                    MessageBox.Show("Hasta.");
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            int selectedRowIndex = dataGridView1.SelectedCells[0].RowIndex;
+            int hastaID = Convert.ToInt32(dataGridView1.Rows[selectedRowIndex].Cells["tc_kimlik"].Value);
+
+            // Güncellenecek hasta bilgilerini
+            string ad = textBox1.Text;
+            string soyad = textBox2.Text;
+            DateTime dogumTarihi = dateTimePicker1.Value;
+            string dogumYeri = textBox5.Text;
+            string tc_kimlik = textBox3.Text;
+            string telefon = textBox6.Text;
+            string hastalik = textBox7.Text;
+            string ilac = textBox8.Text;
+            string adres = textBox4.Text;
+
+            HastaGuncelle(hastaID, ad, soyad, dogumTarihi, dogumYeri, tc_kimlik, telefon, hastalik, ilac, adres);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[rowIndex];
+
+            string ad = selectedRow.Cells["ad"].Value.ToString();
+            string soyad = selectedRow.Cells["soyad"].Value.ToString();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string yeniAd = textBox1.Text;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string yeniSoyad = textBox2.Text;
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string tc_kimlik = textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            string Adres = textBox4.Text;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            string Dogum_Yeri = textBox5.Text;
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            string hastalik = textBox7.Text;
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            string ilac = textBox8.Text;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime seciliTarih = dateTimePicker1.Value;
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            string yeniTelefon = textBox6.Text;
+        }
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string ad = textBox1.Text;
+            string soyad = textBox2.Text;
+            string Adres = textBox4.Text;
+            DateTime dogumTarihi = dateTimePicker1.Value;
+            string dogumYeri = textBox5.Text;
+            string tc_kimlik = textBox3.Text;
+            string telefon = textBox6.Text;
+            string hastalik = textBox7.Text;
+            string ilac = textBox8.Text;
+
+            HastaEkle(ad, soyad, dogumTarihi, dogumYeri, tc_kimlik, telefon, hastalik, ilac, Adres);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+            MessageBox.Show("Hasta Kayıt Sistemine Hoş Geldiniz!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Tc no bilgisi alma
+            string tcKimlikNo = textBox9.Text;
+
+            // Tc kimlik no su girilen hastanın bilgilerini textBox lara ekleme
+            using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("SELECT * FROM hasta WHERE tc_kimlik = @tc_kimlik", baglanti))
+            {
+                adapter.SelectCommand.Parameters.AddWithValue("@tc_kimlik", tcKimlikNo);
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    
+                    DataRow row = dt.Rows[0];
+                    textBox1.Text = row["ad"].ToString();
+                    textBox2.Text = row["soyad"].ToString();
+                    dateTimePicker1.Value = Convert.ToDateTime(row["dogum_tarihi"]);
+                    textBox5.Text = row["dogum_yeri"].ToString();
+                    textBox3.Text = row["tc_kimlik"].ToString();
+                    textBox6.Text = row["telefon"].ToString();
+                    textBox7.Text = row["hastalik"].ToString();
+                    textBox8.Text = row["ilac"].ToString();
+                    textBox4.Text = row["adres"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Hasta bulunamadı.");
+                }
+            }
+        }
+
+        
+    }
+}
+
